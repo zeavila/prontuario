77897e3cf800d7b9ddde7ae311966d551ae82252
José Alberto <zeavila@gmail.com>
2019-12-22 18:18:56 -0300

    Added documentations, Functions Create, Update and remove

--- backend/src/daos/patientDAO.js
+++ backend/src/daos/patientDAO.js
@@ -1,11 +1,20 @@
 const dbServer = require("../servers/dbServer");
+const moment = require("moment");
 
-//READ
+/**
+ * Read of Patients
+ * @param {Object} pConditions
+ * @returns {Promise} Result os query or error
+ */
 const read = (pConditions = null) => {
   return new Promise((pResolve, pReject) => {
-    const xSQL = "SELECT * FROM patient";
+    let xSQL = "SELECT * FROM patient";
+    if (pConditions != null && pConditions.patient_id) {
+      xSQL += ` WHERE patient_id = ${pConditions.patient_id}`;
+    }
     return dbServer.getConnection().then(pCN => {
       return pCN.query(xSQL, (pError, pResult) => {
+        pCN.end();
         if (pError) {
           return pReject(pError);
         } else {
@@ -16,13 +25,90 @@ const read = (pConditions = null) => {
   });
 };
 
-//CREATE
-// const create = pDataObject => {};
+/**
+ * Create of Patient
+ * @param {Object} pDataObject Data of Patient {name, telephone, birth_date, gender, height, weight}
+ * @returns {Promise} Message of Sucess or Error
+ */
+const create = pDataObject => {
+  return new Promise((pResolve, pReject) => {
+    const xSQL =
+      "INSERT INTO patient (`name`, `telephone`, `birth_date`, `gender`, `height`, `weight`) VALUES (?)";
+    const xValue = [
+      [
+        pDataObject.name,
+        pDataObject.telephone,
+        pDataObject.birth_date,
+        pDataObject.gender,
+        pDataObject.height,
+        pDataObject.weight
+      ]
+    ];
+    return dbServer.getConnection().then(pCN => {
+      return pCN.query(xSQL, xValue, (pError, pResult) => {
+        if (pError) {
+          return pReject(pError);
+        } else {
+          return pResolve({ message: "Successsfully Created Patient" });
+        }
+      });
+    });
+  });
+};
 
-//UPDATE
-// const update = (pDataObject, pConditions) => {};
+/**
+ * Update data of one patient
+ * @param {Object} pDataObject Data of Patient
+ * @param {Object} pConditions ID of Patient {patient_id}
+ * @returns {Promise} Message of Sucess or Error
+ */
+const update = (pDataObject, pConditions) => {
+  return new Promise((pResolve, pReject) => {
+    return read(pConditions)
+      .then(rPatient => {
+        if (!rPatient || !rPatient[0]) {
+          return pReject({ message: "Patient not found" });
+        }
+        return Promise.all([dbServer.getConnection(), rPatient[0]]);
+      })
+      .then(([pCN, pPatient]) => {
+        const xMerged = { ...pPatient, ...pDataObject };
+        const xBirthDate = moment(xMerged.birth_date).format("YYYY-MM-DD");
+        let xSQL = "UPDATE patient ";
+        xSQL += ` SET name = '${xMerged.name}', telephone = '${xMerged.telephone}', `;
+        xSQL += ` birth_date = '${xBirthDate}', gender = '${xMerged.gender}', `;
+        xSQL += ` height = ${xMerged.height}, weight = ${xMerged.weight}`;
+        xSQL += ` WHERE patient_id = ${pConditions.patient_id}`;
+        return pCN.query(xSQL, (pError, pResult) => {
+          if (pError) {
+            return pReject(pError);
+          } else {
+            return pResolve({ message: "Successsfully Updated Patient" });
+          }
+        });
+      });
+  });
+};
 
-//DELETE
-// const delete = pConditions => {};
+/**
+ * Remove one Patient
+ * @param {Object} pConditions ID of Patient {patient_id}
+ * @returns {Promise}
+ */
+const remove = pConditions => {
+  return new Promise((pResolve, pReject) => {
+    const xSQL = "DELETE FROM patient WHERE patient_id = ?";
+    return dbServer.getConnection().then(pCN => {
+      return pCN.query(xSQL, pConditions.patient_id, (pError, pResult) => {
+        pCN.end();
+        if (pError) {
+          return pReject(pError);
+        } else {
+          return pResolve({ message: "Patient removed." });
+        }
+      });
+    });
+  });
+};
 
-module.exports = { read };
+module.exports = { read, create, update, remove };
