4afaedb3bc4aca28e9a9efcdb1eed55af2221da0
José Alberto <zeavila@gmail.com>
2019-12-22 14:58:34 -0300

    Enum of routes

---
+++ backend/src/enums/ROUTES.js
@@ -0,0 +1,9 @@
+//Definição das Rotas
+const BASE_URL_V1 = "/api/v1";
+
+module.exports = {
+  BASE_URL: BASE_URL_V1,
+  PATIENTS_URL: BASE_URL_V1 + "/patients",
+  SCHEDULES_URL: BASE_URL_V1 + "/schedules",
+  NOTES_URL: BASE_URL_V1 + "/notes"
+};
