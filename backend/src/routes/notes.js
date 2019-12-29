ce726cafa29131284522da49280b5911c24b9ade
José Alberto <zeavila@gmail.com>
2019-12-29 19:07:56 -0300

    Created Read Note

--- backend/src/routes/notes.js
+++ backend/src/routes/notes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const dao = require("../daos/noteDAO");
 
 /**
- * Read One Note by ID
+ * Read Note by Patient ID
  */
-router.get("/:id", (pReq, pRes) => {
+router.get("/:id/:date", (pReq, pRes) => {
   //Route Validation
   if (!pReq.params.id) {
     return pRes.status(400).send({
@@ -14,8 +14,16 @@ router.get("/:id", (pReq, pRes) => {
       }
     });
   }
+  if (!pReq.params.date) {
+    return pRes.status(400).send({
+      data: {
+        message: "Date of Note is required.",
+        status: 400
+      }
+    });
+  }
   return dao
-    .read({ patient_id: pReq.params.id })
+    .read({ patient_id: pReq.params.id, schedule_date: pReq.params.date })
     .then(rResult => {
       return pRes.send({ data: rResult });
     })
@@ -31,20 +39,20 @@ router.get("/:id", (pReq, pRes) => {
 });
 
 /**
- * List of All Notes of one Patient
+ * Read Note by Patient ID
  */
-router.get("/", (pReq, pRes) => {
+router.get("/:id", (pReq, pRes) => {
   //Route Validation
-  if (!pReq.body.patient_id) {
+  if (!pReq.params.id) {
     return pRes.status(400).send({
       data: {
-        message: "ID of Patient is required.",
+        message: "ID of Note is required.",
         status: 400
       }
     });
   }
   return dao
-    .read(pReq.body)
+    .read({ patient_id: pReq.params.id })
     .then(rResult => {
       return pRes.send({ data: rResult });
     })
@@ -102,7 +110,6 @@ router.post("/", (pReq, pRes) => {
       return pRes.send({ data: rResult });
     })
     .catch(rErr => {
-      console.log(rErr);
       return pRes.status(400).send({
         data: {
           message: rErr.message,
